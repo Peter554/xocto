a7919f7f0c3a1636ee35095999257c0ad8319953
Peter Byfield <peter.byfield@kraken.tech>
2025-03-03 13:05:42 +0100

    Add workflow to benchmark main

--- benchmarking/scripts/extract_benchmarks.py
+++ benchmarking/scripts/extract_benchmarks.py
@@ -8,21 +8,27 @@ import polars as pl
 import pytz
 
 
+def _parse_datetime(s: str) -> datetime.datetime:
+    return datetime.datetime.fromisoformat(s).astimezone(pytz.UTC)
+
+
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--input-json", "-i", required=True)
-    arg_parser.add_argument("--output-dir", "-o", required=True)
-    arg_parser.add_argument("--testbed", "-m", required=True)
+    arg_parser.add_argument("--input", "-i", required=True)
+    arg_parser.add_argument("--output", "-o", required=True)
+    arg_parser.add_argument("--testbed", "-t", required=True)
     args = arg_parser.parse_args()
 
-    with open(args.input_json) as f:
+    with open(args.input) as f:
         benchmarks_json = json.load(f)
 
-    timestamp = datetime.datetime.fromisoformat(benchmarks_json["datetime"]).astimezone(
-        pytz.UTC
-    )
+    timestamp = _parse_datetime(benchmarks_json["datetime"])
     commit_sha = benchmarks_json["commit_info"]["id"]
     commit_branch = benchmarks_json["commit_info"]["branch"]
+    commit_timestamp = _parse_datetime(benchmarks_json["commit_info"]["time"])
+    commit_author_timestamp = _parse_datetime(
+        benchmarks_json["commit_info"]["author_time"]
+    )
 
     rows = []
     for benchmark in benchmarks_json["benchmarks"]:
@@ -32,10 +38,14 @@ def main():
                 "timestamp": timestamp.isoformat(),
                 "commit_sha": commit_sha,
                 "commit_branch": commit_branch,
+                "commit_timestamp": commit_timestamp.isoformat(),
+                "commit_author_timestamp": commit_author_timestamp.isoformat(),
                 "benchmark_name": benchmark["name"],
                 "benchmark_fullname": benchmark["fullname"],
-                "benchmark_mean_s": benchmark["stats"]["mean"],
-                "benchmark_stddev_s": benchmark["stats"]["stddev"],
+                "benchmark_mean_s": round(benchmark["stats"]["mean"], 9),
+                "benchmark_stddev_s": round(benchmark["stats"]["stddev"], 9),
+                "benchmark_median_s": round(benchmark["stats"]["median"], 9),
+                "benchmark_iqr_s": round(benchmark["stats"]["iqr"], 9),
                 "benchmark_rounds": benchmark["stats"]["rounds"],
                 "benchmark_iterations": benchmark["stats"]["iterations"],
             }
@@ -43,10 +53,7 @@ def main():
 
     df = pl.DataFrame(data=rows, orient="row")
 
-    output_path = (
-        pathlib.Path(args.output_dir)
-        / f"{commit_sha}_{timestamp.strftime('%Y%m%d%H%M%S')}.csv"
-    )
+    output_path = pathlib.Path(args.output)
     os.makedirs(output_path.parent, exist_ok=True)
     df.write_csv(output_path)
 
